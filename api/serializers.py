33ea4ee13b7b6ad988b5a3edd67fda0d21ba60e8
Luan fonseca de farias <luanfonceca@gmail.com>
2015-12-28 18:20:03 -0300

    Using base64 to handle the image upload

--- api/serializers.py
+++ api/serializers.py
@@ -10,6 +10,46 @@ from language.models import Language
 from photo.models import Photo
 
 
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        from django.core.files.base import ContentFile
+        import base64
+        import six
+        import uuid
+
+        # Check if this is a base64 string
+        if isinstance(data, six.string_types):
+            # Check if the base64 string is in the "data:" format
+            if 'data:' in data and ';base64,' in data:
+                # Break out the header from the base64 content
+                header, data = data.split(';base64,')
+
+            # Try to decode the file. Return validation error if it fails.
+            try:
+                decoded_file = base64.b64decode(data)
+            except TypeError:
+                self.fail('invalid_image')
+
+            # Generate file name:
+            file_name = str(uuid.uuid4())[:12] # 12 characters are more than enough.
+            # Get the file name extension:
+            file_extension = self.get_file_extension(file_name, decoded_file)
+
+            complete_file_name = "%s.%s" % (file_name, file_extension, )
+
+            data = ContentFile(decoded_file, name=complete_file_name)
+
+        return super(Base64ImageField, self).to_internal_value(data)
+
+    def get_file_extension(self, file_name, decoded_file):
+        import imghdr
+
+        extension = imghdr.what(file_name, decoded_file)
+        extension = "jpg" if extension == "jpeg" else extension
+
+        return extension
+
+
 class InterestSerializer(serializers.ModelSerializer):
     url = serializers.CharField(source='get_image_url', read_only=True)
     slug = serializers.CharField(source='image_class', read_only=True)
@@ -94,6 +134,9 @@ class ProfileSerializer(serializers.ModelSerializer):
         read_only=True
     )
     photos = PhotoUrlSerializer(many=True)
+    photo = Base64ImageField(
+        max_length=None, use_url=True,
+    )
 
     class Meta:
         model = Profile
