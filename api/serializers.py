7e5e05197cacefa878225d13cf028b319358621d
Luan fonseca de farias <luanfonceca@gmail.com>
2016-02-16 16:43:44 -0300

    Fix the Validation condition.

--- api/serializers.py
+++ api/serializers.py
@@ -78,7 +78,7 @@ class ReferenceSerializer(serializers.ModelSerializer):
     def validate_users_are_mate(self, data):
         from_user = data.get('from_user')
         to_user = data.get('to_user')
-        if from_user.mates_users.filter(profile=to_user).exists():
+        if not from_user.mates_users.filter(profile=to_user).exists():
             message = ('You can only create references for your mates.')
             raise serializers.ValidationError(message)
 
