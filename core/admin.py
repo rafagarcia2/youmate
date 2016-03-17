d31d660bc5f461808beda50822963083ddf3abe9
Luan fonseca de farias <luanfonceca@gmail.com>
2016-03-17 19:09:31 -0300

    Add social auth config and coreuser in admin.

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from core.models import Profile
+from core.models import CoreUser, Profile
+
+
+class CoreUserAdmin(admin.ModelAdmin):
+    pass
 
 
 class ProfileAdmin(admin.ModelAdmin):
     pass
 
+admin.site.register(CoreUser, CoreUserAdmin)
 admin.site.register(Profile, ProfileAdmin)
--- youmate/settings.py
+++ youmate/settings.py
@@ -183,7 +183,7 @@ AUTHENTICATION_BACKENDS = global_settings.AUTHENTICATION_BACKENDS + (
     'rest_framework_social_oauth2.backends.DjangoOAuth2',
 )
 
-AUTH_USER_MODEL = 'core.CoreUser'
+SOCIAL_AUTH_USER_MODEL = AUTH_USER_MODEL = 'core.CoreUser'
 
 # Social auth configuration
 SOCIAL_AUTH_FACEBOOK_KEY = '1685351455020151'
