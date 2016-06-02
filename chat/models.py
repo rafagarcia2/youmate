52f4675b1e8288da3f3b7821638fe82cd9ee8e9b
Rafael Garcia <rafaelgarcia2013@gmail.com>
2016-06-02 15:40:44 -0300

    Correção no espaçamento

--- chat/models.py
+++ chat/models.py
@@ -4,12 +4,11 @@ from django.db import models
 from django_extensions.db.fields import CreationDateTimeField
 
 class Chat(models.Model):
-	text = models.CharField('mensagem', max_length=400)
-	pub_date = CreationDateTimeField() # Verificar com datetime
-	from_user = models.ForeignKey(to='core.Profile', related_name='chatsFromUser')
-	to_user = 	models.ForeignKey(to='core.Profile', related_name='chatsToUser')
+    text = models.CharField('mensagem', max_length=400)
+    pub_date = CreationDateTimeField() # Verificar com datetime
+    from_user = models.ForeignKey(to='core.Profile', related_name='chats_from')
+    to_user = models.ForeignKey(to='core.Profile', related_name='chats_to')
 
-
-	class Meta:
-		verbose_name = "Chat"
+    class Meta:
+        verbose_name = "Chat"
         verbose_name_plural = "Chats"
--- chat/serializers.py
+++ chat/serializers.py
@@ -1,8 +1,6 @@
 from rest_framework import serializers
-
 from chat.models import Chat
 
-
 class ChatSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Chat
+    class Meta:
+        model = Chat
