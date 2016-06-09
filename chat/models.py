aeeb3cb093642d8a719e3c8e0ff03010036be57a
Rafael Garcia <rafaelgarcia2013@gmail.com>
2016-06-09 18:04:09 -0300

    Add model Message

--- chat/models.py
+++ chat/models.py
@@ -4,8 +4,7 @@ from django.db import models
 from django_extensions.db.fields import CreationDateTimeField
 
 class Chat(models.Model):
-    text = models.CharField('mensagem', max_length=400)
-    pub_date = CreationDateTimeField() # Verificar com datetime
+    pub_date = CreationDateTimeField()
     from_user = models.ForeignKey(to='core.Profile', related_name='chats_from')
     to_user = models.ForeignKey(to='core.Profile', related_name='chats_to')
 
@@ -13,10 +12,12 @@ class Chat(models.Model):
         verbose_name = "Chat"
         verbose_name_plural = "Chats"
 
-    # def get_chat(self): # Peguar minha conversas
-    #     return self.chat.filter(
-    #         to_user__from_user=self)
+class Message(models.Model):
+    text = models.CharField('mensagem', max_length=400)
+    pub_date = CreationDateTimeField()
+    from_user = models.ForeignKey(to='core.Profile', related_name='message_from')
+    to_chat = models.ForeignKey(to='chat.Chat', related_name='message_to') 
 
-    # def get_chat_with_me(self): 
-    #     return self.chat_with_me.filter(
-    #         from_user__to_user=self)
+    class Meta:
+        verbose_name = "Message"
+        verbose_name_plural = "Messages"
--- chat/views.py
+++ chat/views.py
@@ -1,35 +1,47 @@
 from rest_framework import generics
 
-from chat.models import Chat
+from chat.models import Chat, Message
 from chat import serializers
 
-# Teste Inicial, nada fixo ainda, tudo pode ser substituido.
+from rest_framework.response import Response
+from rest_framework.exceptions import NotAuthenticated
+from django.core.exceptions import ObjectDoesNotExist
 
 class ChatMixin(object):
     queryset = Chat.objects.all()
     serializer_class = serializers.ChatSerializer
 
+class MessageMixin(object):
+    queryset = Message.objects.all()
+    serializer_class = serializers.MessageSerializer
 
 class ChatList(ChatMixin, generics.ListCreateAPIView):
     '''View para visualizar todas as conversas do user.'''
     def get_queryset(self):
-        queryset = super(ChatList, self).get_queryset()
         if not self.request.user.is_authenticated():
             raise NotAuthenticated()
         from_user = self.request.user.profile
 
-        queryset = self.queryset.filter(from_user=from_user) | self.queryset.filter(to_user=from_user)
+        queryset = super(ChatList, self).get_queryset()
+        queryset = self.queryset.filter(Q(from_user=from_user) | Q(to_user=from_user))
+
         return queryset
 
-class ChatView(ChatMixin, generics.ListCreateAPIView):
-    '''View para visualizar cada conversa em particular.'''
+class MessageView(MessageMixin, generics.ListCreateAPIView):
     def get_queryset(self):
-        queryset = super(ChatList, self).get_queryset()
         if not self.request.user.is_authenticated():
             raise NotAuthenticated()
         from_user = self.request.user.profile
 
-        to_user = Profile.objects.get(pk=self.kwargs.get('to_user'))
-        queryset = self.queryset.filter(from_user=from_user, to_user=to_user) | self.queryset.filter(from_user=to_user, to_user=from_user)
+        queryset = super(MessageView, self).get_queryset()
+        try:
+            to_chat = Chat.objects.get(pk=self.kwargs.get('to_chat'))
+        except ObjectDoesNotExist:
+            return None
 
+        # Testing user in chat
+        if (to_chat.from_user != from_user and to_chat.to_user != from_user):
+            return None
+
+        queryset = self.queryset.filter(to_chat=to_chat)
         return queryset
