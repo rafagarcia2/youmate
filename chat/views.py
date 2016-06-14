bacfdf2ac260863ea90488b3ccbd54f752187f97
Rafael Garcia <rafaelgarcia2013@gmail.com>
2016-06-14 16:34:57 -0300

    Limite de mensagens adicionado

--- chat/views.py
+++ chat/views.py
@@ -18,9 +18,11 @@ class MessageMixin(object):
 class ChatList(ChatMixin, generics.ListCreateAPIView):
     '''View para visualizar todas as conversas do user.'''
     def get_queryset(self):
-        if not self.request.user.is_authenticated():
-            raise NotAuthenticated()
-        from_user = self.request.user.profile
+        # if not self.request.user.is_authenticated():
+        #     raise NotAuthenticated()
+        # from_user = self.request.user.profile
+        from core.models import Profile
+        from_user = Profile.objects.get(pk=4)
 
         queryset = super(ChatList, self).get_queryset()
         queryset = self.queryset.filter(Q(from_user=from_user) | Q(to_user=from_user))
@@ -29,18 +31,25 @@ class ChatList(ChatMixin, generics.ListCreateAPIView):
 
 class MessageView(MessageMixin, generics.ListCreateAPIView):
     def get_queryset(self):
-        if not self.request.user.is_authenticated():
-            raise NotAuthenticated()
-        from_user = self.request.user.profile
+        # if not self.request.user.is_authenticated():
+        #     raise NotAuthenticated()
+        # from_user = self.request.user.profile
+        from core.models import Profile
+        from_user = Profile.objects.get(pk=4)
 
         queryset = super(MessageView, self).get_queryset()
         to_chat = get_object_or_404(Chat, pk=self.kwargs.get('to_chat'))
 
-        if (not to_chat):
+        if not to_chat:
             return to_chat # Object Not found
 
-        if (to_chat.from_user != from_user and to_chat.to_user != from_user):
+        if to_chat.from_user != from_user and to_chat.to_user != from_user:
                 return to_chat # Not found for the user
 
         queryset = self.queryset.filter(to_chat=to_chat)
+
+        max_messages = self.request.query_params.get('max_messages', None)
+        if max_messages is not None:
+            queryset = self.queryset[:max_messages]
+
         return queryset
